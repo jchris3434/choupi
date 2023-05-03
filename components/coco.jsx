6b7ab6c09014c72b59046f6bf36914875f74bec4
jchris3434 <jc.fontaine34@gmail.com>
2023-05-03 10:37:19 +0000

    base app choupi

---
+++ components/coco.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/coco.module.css';
+
+function Coconut() {
+  return (
+    <section>
+        <h5 className={styles.appName}>Choupi' App</h5>
+    </section>
+  );
+}
+
+export default Coconut;
---
+++ components/painting.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import styles from '../styles/coco.module.css';
+
+function Painting() {
+  return (
+    <section>
+        <div>PAINTING BOARD</div>
+    </section>
+  );
+}
+
+export default Painting;
